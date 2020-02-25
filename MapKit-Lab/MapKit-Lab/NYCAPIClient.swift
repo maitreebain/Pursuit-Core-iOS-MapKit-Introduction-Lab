062afb4df493b5512108b9351fe1799ac35aa772
maitreebain <maitreebain@gmail.com>
2020-02-25 17:48:46 -0500

    working on model and api client

---
+++ MapKit-Lab/MapKit-Lab/NYCAPIClient.swift
@@ -0,0 +1,17 @@
+//
+//  NYCAPIClient.swift
+//  MapKit-Lab
+//
+//  Created by Maitree Bain on 2/25/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+struct NYCAPIClient {
+    
+    static func getNYCData(completion @escaping(Result <>) -> ()) {
+        
+    }
+}
